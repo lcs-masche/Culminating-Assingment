7bf3ecc771e64540b8a501fb34f469fb2c3a7d28
Moritz Asche <moritz@asches.de>
2021-03-03 17:09:16 -0500

    Initial Commit

---
+++ Culminating Assingment/Culminating_AssingmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Culminating_AssingmentApp.swift
+//  Culminating Assingment
+//
+//  Created by Mo Asche on 2021-03-03.
+//
+
+import SwiftUI
+
+@main
+struct Culminating_AssingmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
