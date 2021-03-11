b45eaaa6b07b0f0eeedb935554ab0a4a21bed41f
Moritz Asche <moritz@asches.de>
2021-03-11 09:23:49 -0500

    the list view that I planned to do looks ugly. I am now doing rectangles that ''float"

--- Culminating Assingment/Views/ContentView.swift
+++ Culminating Assingment/Views/ContentView.swift
@@ -17,35 +17,68 @@ struct ListItem: Identifiable {
 struct ContentView: View {
     var body: some View {
         
-    
-        NavigationView {
-            
-            
-
-            List {
-                NavigationLink(destination: DaysTillDeath()) {
-
-                ListItemView(title: "Days Till You Die", caption: "This gives you an estimate on when you will die")
-                }
+        ZStack {
+            Rectangle()
+                .foregroundColor(.red)
+                .frame(width: 170, height: 35)
+                .cornerRadius(3.0)
+                .offset(x: 0, y: -220)
+                        
+            Text("Death")
+                .fontWeight(.bold)
+                .foregroundColor(Color.black)
+                .colorMultiply(.black)
+                .frame(width: 100, height: 20)
+                .offset(x: 0, y: -220)
+                .font(.title)
                 
-                NavigationLink(destination: DaysTillBDay()) {
-
-                ListItemView(title: "Days Till Your B-Day", caption: "This gives you the days left until ur B-Day")
-                
-                }
-
-                NavigationLink(destination: DaysTillHoliday()) {
-
-                    ListItemView(title: "Days Till The Next Holiday", caption: "This gives you the days left until the next break")
-                }
-
-                NavigationLink(destination: DaysLivedSinceBirth()) {
-
-                    ListItemView(title: "Days Passed Since Your Birth", caption: "The days passed since your birth")
-                }
-
-            }
         }
+        
+        ZStack {
+            Rectangle()
+                .foregroundColor(.red)
+                .frame(width: 170, height: 35)
+                .cornerRadius(3.0)
+                .offset(x: 0, y: -180)
+                        
+            Text("B-Day")
+                .fontWeight(.bold)
+                .foregroundColor(Color.black)
+                .colorMultiply(.black)
+                .frame(width: 100, height: 20)
+                .offset(x: 0, y: -180)
+                .font(.title)
+                
+        }
+
+        
+        
+//        NavigationView {
+            
+//            List {
+//                NavigationLink(destination: DaysTillDeath()) {
+//
+//                ListItemView(title: "Days Till You Die", caption: "This gives you an estimate on when you will die")
+//                }
+//
+//                NavigationLink(destination: DaysTillBDay()) {
+//
+//                ListItemView(title: "Days Till Your B-Day", caption: "This gives you the days left until ur B-Day")
+//
+//                }
+//
+//                NavigationLink(destination: DaysTillHoliday()) {
+//
+//                    ListItemView(title: "Days Till The Next Holiday", caption: "This gives you the days left until the next break")
+//                }
+//
+//                NavigationLink(destination: DaysLivedSinceBirth()) {
+//
+//                    ListItemView(title: "Days Passed Since Your Birth", caption: "The days passed since your birth")
+//                }
+//
+//            }
+//        }
     }
 }
 
