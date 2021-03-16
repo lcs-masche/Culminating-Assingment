967619131c1217e12399fb500d8d818825da40ad
Moritz Asche <moritz@asches.de>
2021-03-16 14:33:31 -0400

    Commented omg previous work, now working with navigation links and navigationTilte and so on

--- Culminating Assingment/Culminating_AssingmentApp.swift
+++ Culminating Assingment/Culminating_AssingmentApp.swift
@@ -11,7 +11,10 @@ import SwiftUI
 struct Culminating_AssingmentApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                ContentView()
+            }
+
         }
     }
 }
--- Culminating Assingment/Views/ContentView.swift
+++ Culminating Assingment/Views/ContentView.swift
@@ -19,181 +19,193 @@ struct ContentView: View {
     
     @State private var showDetails = true
     
+    
     var body: some View {
-        
-        ZStack {
+    
+        VStack {
             
-            Rectangle()
-                .colorMultiply(.black)
-//                .foregroundColor(.init(red: 7, green: 238, blue: 242, opacity: 100))
-                .frame(width: 1000, height: 1000, alignment: .center)
-        
-            
-            Text("TIME TILL . . .")
-                .fontWeight(.heavy)
-                .foregroundColor(.white)
-                .colorMultiply(.white)
-                .multilineTextAlignment(.leading)
-                .offset(x: -40, y: -250)
-                .font(.largeTitle)
-            
-            Rectangle()
-                .foregroundColor(.white)
-                .opacity(0.75)
-                .frame(width: 200, height: 430)
-                .cornerRadius(10.0)
-                .offset(x: 0, y: 0)
-
-            
-        
-            ZStack {
-                Rectangle()
-                    .foregroundColor(.red)
-                    .frame(width: 170, height: 35)
-                    .cornerRadius(3.0)
-                    .offset(x: 0, y: -180)
-                    
-                    
-
-                
-                Button("Show details") {
-        
-                    showDetails.toggle()
-                            }
-                if showDetails {
-                    Text("Death")
-                        .fontWeight(.bold)
-                        .foregroundColor(Color.black)
-                        .colorMultiply(.black)
-                        .frame(width: 100, height: 20)
-                        .offset(x: 0, y: -180)
-                        .font(.title)
-                }
-
-                    
-            }
-            
-            ZStack {
-                Rectangle()
-                    .foregroundColor(.red)
-                    .frame(width: 170, height: 35)
-                    .cornerRadius(3.0)
-                    .offset(x: 0, y: -130)
-                            
+            NavigationLink(destination: DaysTillBDay()) {
                 Text("B-Day")
                     .fontWeight(.bold)
                     .foregroundColor(Color.black)
                     .colorMultiply(.black)
                     .frame(width: 100, height: 20)
-                    .offset(x: 0, y: -130)
+//                    .offset(x: 0, y: -130)
                     .font(.title)
-                    
-            }
-            
-            ZStack {
-                Rectangle()
-                    .foregroundColor(.red)
-                    .frame(width: 170, height: 35)
-                    .cornerRadius(3.0)
-                    .offset(x: 0, y: -80)
-                            
-                Text("Holiday")
-                    .fontWeight(.bold)
-                    .foregroundColor(Color.black)
-                    .colorMultiply(.black)
-                    .frame(width: 100, height: 20)
-                    .offset(x: 0, y: -80)
-                    .font(.title)
-                    
-            }
-            
-            ZStack {
-                Rectangle()
-                    .foregroundColor(.red)
-                    .frame(width: 170, height: 35)
-                    .cornerRadius(3.0)
-                    .offset(x: 0, y: -30)
-                            
-                Text("Days Lived")
-                    .fontWeight(.bold)
-                    .foregroundColor(Color.black)
-                    .colorMultiply(.black)
-                    .frame(width: 100, height: 20)
-                    .offset(x: 0, y: -30)
-                    .font(.title)
-                    
-            }
-            
-            ZStack {
-                Rectangle()
-                    .foregroundColor(.red)
-                    .frame(width: 170, height: 35)
-                    .cornerRadius(3.0)
-                    .offset(x: 0, y: 20)
-                            
-                Text("B-Day")
-                    .fontWeight(.bold)
-                    .foregroundColor(Color.black)
-                    .colorMultiply(.black)
-                    .frame(width: 100, height: 20)
-                    .offset(x: 0, y: 20)
-                    .font(.title)
-                    
-            }
-        
-            ZStack {
-                Rectangle()
-                    .foregroundColor(.red)
-                    .frame(width: 170, height: 35)
-                    .cornerRadius(3.0)
-                    .offset(x: 0, y: 20)
-                            
-                Text("Year")
-                    .fontWeight(.bold)
-                    .foregroundColor(Color.black)
-                    .colorMultiply(.black)
-                    .frame(width: 100, height: 20)
-                    .offset(x: 0, y: 20)
-                    .font(.title)
-                    
             }
 
-            
-            
-            
         }
+        .navigationTitle("Time Till")
         
         
-
-
-        
-        
-//        NavigationView {
-            
-//            List {
-//                NavigationLink(destination: DaysTillDeath()) {
+//        ZStack {
 //
-//                ListItemView(title: "Days Till You Die", caption: "This gives you an estimate on when you will die")
-//                }
+//            Rectangle()
+//                .colorMultiply(.black)
+//                //                .foregroundColor(.init(red: 7, green: 238, blue: 242, opacity: 100))
+//                .frame(width: 1000, height: 1000, alignment: .center)
 //
-//                NavigationLink(destination: DaysTillBDay()) {
 //
-//                ListItemView(title: "Days Till Your B-Day", caption: "This gives you the days left until ur B-Day")
+//            Text("TIME TILL . . .")
+//                .fontWeight(.heavy)
+//                .foregroundColor(.white)
+//                .colorMultiply(.white)
+//                .multilineTextAlignment(.leading)
+//                .offset(x: -40, y: -250)
+//                .font(.largeTitle)
 //
-//                }
+//            Rectangle()
+//                .foregroundColor(.white)
+//                .opacity(0.75)
+//                .frame(width: 200, height: 430)
+//                .cornerRadius(10.0)
+//                .offset(x: 0, y: 0)
 //
-//                NavigationLink(destination: DaysTillHoliday()) {
 //
-//                    ListItemView(title: "Days Till The Next Holiday", caption: "This gives you the days left until the next break")
-//                }
 //
-//                NavigationLink(destination: DaysLivedSinceBirth()) {
+//            ZStack {
+//                Rectangle()
+//                    .foregroundColor(.red)
+//                    .frame(width: 170, height: 35)
+//                    .cornerRadius(3.0)
+//                    .offset(x: 0, y: -180)
 //
-//                    ListItemView(title: "Days Passed Since Your Birth", caption: "The days passed since your birth")
-//                }
+//
+//                Text("Death")
+//                    .fontWeight(.bold)
+//                    .foregroundColor(Color.black)
+//                    .colorMultiply(.black)
+//                    .frame(width: 100, height: 20)
+//                    .offset(x: 0, y: -180)
+//                    .font(.title)
+//
+//                //                Button("Show details") {
+//                //                    showDetails.toggle()
+//                //                            }
+//                //                if showDetails {
+//                //                    NavigationLink(<#T##titleKey: LocalizedStringKey##LocalizedStringKey#>, destination: DaysTillDeath)
+//                //                }
+//
 //
 //            }
+//
+//            ZStack {
+////                Rectangle()
+////                    .foregroundColor(.red)
+////                    .frame(width: 170, height: 35)
+////                    .cornerRadius(3.0)
+////                    .offset(x: 0, y: -130)
+//
+//
+//
+//
+//
+//            }
+//
+//            ZStack {
+//                Rectangle()
+//                    .foregroundColor(.red)
+//                    .frame(width: 170, height: 35)
+//                    .cornerRadius(3.0)
+//                    .offset(x: 0, y: -80)
+//
+//                Text("Holiday")
+//                    .fontWeight(.bold)
+//                    .foregroundColor(Color.black)
+//                    .colorMultiply(.black)
+//                    .frame(width: 100, height: 20)
+//                    .offset(x: 0, y: -80)
+//                    .font(.title)
+//
+//            }
+//
+//            ZStack {
+//                Rectangle()
+//                    .foregroundColor(.red)
+//                    .frame(width: 170, height: 35)
+//                    .cornerRadius(3.0)
+//                    .offset(x: 0, y: -30)
+//
+//                Text("Days Lived")
+//                    .fontWeight(.bold)
+//                    .foregroundColor(Color.black)
+//                    .colorMultiply(.black)
+//                    .frame(width: 100, height: 20)
+//                    .offset(x: 0, y: -30)
+//                    .font(.title)
+//
+//            }
+//
+//            ZStack {
+//                Rectangle()
+//                    .foregroundColor(.red)
+//                    .frame(width: 170, height: 35)
+//                    .cornerRadius(3.0)
+//                    .offset(x: 0, y: 20)
+//
+//                Text("B-Day")
+//                    .fontWeight(.bold)
+//                    .foregroundColor(Color.black)
+//                    .colorMultiply(.black)
+//                    .frame(width: 100, height: 20)
+//                    .offset(x: 0, y: 20)
+//                    .font(.title)
+//
+//            }
+//
+//            ZStack {
+//                Rectangle()
+//                    .foregroundColor(.red)
+//                    .frame(width: 170, height: 35)
+//                    .cornerRadius(3.0)
+//                    .offset(x: 0, y: 20)
+//
+//                Text("Year")
+//                    .fontWeight(.bold)
+//                    .foregroundColor(Color.black)
+//                    .colorMultiply(.black)
+//                    .frame(width: 100, height: 20)
+//                    .offset(x: 0, y: 20)
+//                    .font(.title)
+//
+//            }
+//
+//
+//
+//
 //        }
+//
+//
+//
+//
+//
+//
+//        //        NavigationView {
+//
+//        //            List {
+//        //                NavigationLink(destination: DaysTillDeath()) {
+//        //
+//        //                ListItemView(title: "Days Till You Die", caption: "This gives you an estimate on when you will die")
+//        //                }
+//        //
+//        //                NavigationLink(destination: DaysTillBDay()) {
+//        //
+//        //                ListItemView(title: "Days Till Your B-Day", caption: "This gives you the days left until ur B-Day")
+//        //
+//        //                }
+//        //
+//        //                NavigationLink(destination: DaysTillHoliday()) {
+//        //
+//        //                    ListItemView(title: "Days Till The Next Holiday", caption: "This gives you the days left until the next break")
+//        //                }
+//        //
+//        //                NavigationLink(destination: DaysLivedSinceBirth()) {
+//        //
+//        //                    ListItemView(title: "Days Passed Since Your Birth", caption: "The days passed since your birth")
+//        //                }
+//        //
+//        //            }
+//        //        }
     }
 }
 
